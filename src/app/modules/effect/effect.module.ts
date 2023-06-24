b9531832d10785608a66dba00e0257bb7848fdfc
Sergio Amilcar Lobaton Arcani <lobaton.0722@gmail.com>
2023-06-24 11:38:52 -0400

    list effects implemented

--- src/app/modules/effect/effect.module.ts
+++ src/app/modules/effect/effect.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EffectListComponent } from './pages/effect-list/effect-list.component';
-
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
 import { EffectRoutingModule } from './effect-routing.module';
-
-
 
 @NgModule({
   declarations: [
@@ -12,7 +15,14 @@ import { EffectRoutingModule } from './effect-routing.module';
   ],
   imports: [
     CommonModule,
-    EffectRoutingModule
+    EffectRoutingModule,
+    DynamicDialogModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    InputTextareaModule,
+    InputSwitchModule,
   ]
 })
 export class EffectModule { }
--- src/app/modules/effect/pages/effect-list/effect-list.component.ts
+++ src/app/modules/effect/pages/effect-list/effect-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { Effect } from 'src/app/@core/models/effect';
 import {
   DataTableActionStatus,
@@ -7,6 +9,8 @@ import {
   DataTableSelectionType
 } from 'src/app/@core/types/data-table-definition';
 import { DataTableComponent } from 'src/app/shared/components/data-table/data-table.component';
+import { UpsertEffectFormComponent } from '../../components/upsert-effect-form/upsert-effect-form.component';
+import { EffectService } from 'src/app/@core/services/rest/effect.service';
 
 @Component({
   selector: 'app-effect-list',
@@ -14,6 +18,93 @@ import { DataTableComponent } from 'src/app/shared/components/data-table/data-ta
   styleUrls: ['./effect-list.component.scss']
 })
 export class EffectListComponent {
-  // @ViewChild('clientTable') table!: DataTableComponent<Client>;
+  @ViewChild('effectTable') table!: DataTableComponent<Effect>;
+  ref: DynamicDialogRef | undefined;
 
+  public tableConfig: DataTableConfiguration = {
+    columns: [
+      { title: 'Id', propertyRef: 'id', sortable: false, visible: false },
+      { title: 'Nombre', propertyRef: 'name', sortable: true },
+      { title: 'Descripción', propertyRef: 'description', sortable: false },
+      { title: 'Precio', propertyRef: 'price', sortable: true },
+      { title: 'Activo', propertyRef: 'is_active', type: DataTableColumnType.BOOLEAN },
+      { title: 'Creado', propertyRef: 'created_at', sortable: true, type: DataTableColumnType.DATETIME },
+      { title: 'Actualizado', propertyRef: 'updated_at', sortable: true, type: DataTableColumnType.DATETIME },
+    ],
+    identifierPropRef: 'id',
+    selectionType: DataTableSelectionType.SINGLE,
+    actions: [
+      {
+        title: 'Nuevo',
+        tooltip: 'Nuevo Efecto',
+        icon: 'plus',
+        status: DataTableActionStatus.SUCCESS,
+        selectionConfig: {
+          isRequired: false
+        },
+        callback: () => {
+          this.ref = this.dialogService.open(UpsertEffectFormComponent, { header: 'Crear nuevo Cliente' });
+          this.ref.onClose.subscribe((result) => {
+            if (result) {
+              this.table.reset();
+            }
+          });
+        }
+      },
+      {
+        title: 'Editar',
+        tooltip: 'Editar Cliente',
+        icon: 'pencil',
+        status: DataTableActionStatus.WARNING,
+        selectionConfig: {
+          maxSelectedRows: 1
+        },
+        callback: (selectedRows) => {
+          const client = selectedRows[0];
+          this.ref = this.dialogService.open(UpsertEffectFormComponent, { header: 'Crear nuevo Cliente', data: { client } });
+          this.ref.onClose.subscribe((result) => {
+            if (result) {
+              this.table.reset();
+            }
+          });
+        }
+      },
+      {
+        title: 'Eliminar',
+        tooltip: 'Eliminar Usuario',
+        icon: 'trash',
+        status: DataTableActionStatus.DANGER,
+        selectionConfig: {
+          maxSelectedRows: 1
+        },
+        callback: (selectedRows) => {
+          const clientId = selectedRows[0].id;
+          this.confirmationService.confirm({
+            key: 'confirmDelete',
+            accept: () => {
+              this.effectService.deleteEffect(clientId)
+                .then(() => {
+                  this.messageService.add({ key: 'confirmDelete', severity: 'success', summary: 'Eliminado!', detail: 'El efecto ha sido eliminado!.' });
+                  this.table.reset();
+                })
+                .catch((err) => {
+                  console.error(err);
+                  this.messageService.add({ key: 'confirmDelete', severity: 'error', summary: 'Error', detail: 'No se pudo completar la accion.' })
+                });
+            },
+            reject: () => {
+              this.messageService.add({ key: 'confirmDelete', severity: 'error', summary: 'Cancelado', detail: 'Operacion cancelada!' })
+            }
+          });
+        }
+      }
+    ]
+  };
+
+  constructor(
+    public effectService: EffectService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService,
+    private dialogService: DialogService
+  ){}
 }
