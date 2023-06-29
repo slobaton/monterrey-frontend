8f474db9bfb4b108eee0115178b6bf888741325f
abelopezpaniagua <78995406+abelopezpaniagua@users.noreply.github.com>
2023-06-28 22:24:41 -0400

    add route guards example

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './pages/user-list/user-list.component';
+import { roleGuard } from 'src/app/@core/auth/role.guard';
+import { Role } from 'src/app/@core/enums/role.enum';
 
 const routes: Routes = [
-  { path: '', component: UserListComponent }
+  { path: '', component: UserListComponent, canActivate: [roleGuard([Role.ADMIN])] }
 ];
 
 @NgModule({
--- src/app/modules/wash-type/wash-type-routing.module.ts
+++ src/app/modules/wash-type/wash-type-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WashTypeListComponent } from './pages/wash-type-list/wash-type-list.component';
+import { Role } from 'src/app/@core/enums/role.enum';
+import { roleGuard } from 'src/app/@core/auth/role.guard';
 
 const routes: Routes = [
-  { path: '', component: WashTypeListComponent }
+  { path: '', component: WashTypeListComponent, canActivate: [roleGuard([Role.SECRETARY])] }
 ];
 
 @NgModule({
