81368b3b32db95947d77c377acfa32d4bb8449fc
abelopezpaniagua <78995406+abelopezpaniagua@users.noreply.github.com>
2023-06-28 21:46:23 -0400

    implement access denied page

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { DeniedComponent } from './pages/denied/denied.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'denied', component: DeniedComponent },
   { path: '**', redirectTo: '/notfound' }
 ];
 
