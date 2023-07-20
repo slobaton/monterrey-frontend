1e2ecdcad86871b02df9f3e499fe3c166f8e2ff3
abelopezpaniagua <78995406+abelopezpaniagua@users.noreply.github.com>
2023-07-20 00:33:29 -0400

    update form

--- src/app/@core/models/wash-type.ts
+++ src/app/@core/models/wash-type.ts
@@ -2,6 +2,7 @@ export class WashType {
   constructor(
     public id: number,
     public name: string,
+    public price: number,
     public description: string,
     public is_active: boolean,
     public created_at: string,
