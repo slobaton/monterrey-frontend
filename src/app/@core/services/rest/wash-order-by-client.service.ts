c1dbca4ff53201e2722931230e406bcc476707df
Sergio Lobaton <sergio.lobaton@endeandina.bo>
2024-02-21 13:40:26 -0400

    Implement service to retrieve wash orders by client

---
+++ src/app/@core/services/rest/wash-order-by-client.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { firstValueFrom } from 'rxjs';
+
+import { WashOrder } from '../../models/wash-order';
+import { PaginatedRequest } from '../../models/request/paginated-request';
+import { PaginatedResponse } from '../../models/response/paginated-response';
+import {WashOrderService} from "./wash-order.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WashOrderByClientService extends WashOrderService {
+
+  constructor(_http: HttpClient) {
+    super(_http);
+  }
+
+  override async fetchPaginatedResource(request: PaginatedRequest): Promise<PaginatedResponse<WashOrder>> {
+    try {
+      const includes = ['client', 'wash_type'];
+      const response = await firstValueFrom(
+        this.get<PaginatedResponse<WashOrder>>(
+          `wash-orders/${request.extraParams}/by-client`,
+          this.getPaginationParams(request, 'code', includes)
+        )
+      );
+
+      return response;
+    } catch (error) {
+      return this.handleError(error);
+    }
+  }
+}
